f2e78ecb997ff78466344ba0ac81c444dad3e935
Dalibo <info@skydream.info>
2023-01-15 20:54:07 +0100

    Layout fix
    - Index.vue layout fix

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -1,14 +1,14 @@
+
 <template>
-  <main>
-    <div class="text-center mb-6 text-2xl text-semibold italic">
+  <main class="h-full flex flex-col justify-center mt-6">
+    <div class="text-center mb-6 mx-auto text-2xl text-semibold italic">
       Hello
     </div>
-    <div>
+    <div class="mx-auto">
       <img src="../assets/q.svg">
     </div>
 
   </main>
 </template>
 <script setup lang="ts">
-
 </script>
